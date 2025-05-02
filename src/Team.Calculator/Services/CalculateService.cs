7cacb1e6baf02458d4549c2adfc877a94ed1d4a2
mushtariyy <mushtariyz.1234@gmail.com>
2025-05-02 19:08:31 +0500

    temporary

---
+++ src/Team.Calculator/Services/CalculateService.cs
@@ -0,0 +1,58 @@
+using System.Data;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Calculator.Services;
+
+public class CalculateService : ICalculateService
+{
+    public void Calculate(List<HistoryItem> history)
+    {
+        var historyService = new HistoryService();
+
+        string expression = "";
+        
+        while (true)
+        {
+            try
+            {
+                Console.Write("Enter expression or cancel: ");
+    
+                expression = Console.ReadLine()!.Trim().ToLower();
+    
+                if (string.IsNullOrEmpty(expression))
+                {
+                    Console.WriteLine("Expression cannot be");
+                    continue;
+                }
+                else if (expression == "cancel")
+                {
+                    return;
+                } 
+                var dataTable = new DataTable();
+
+                double result = Convert.ToDouble(dataTable.Compute(expression, null));
+                int id = 1;
+                if(history.Count != 0)
+                {
+                    id = history.Last().Id + 1;
+                }
+                var historyItem = new HistoryItem
+                {
+                    Id = id,
+                    Expression = expression,
+                    Result = result,
+                    CreatedAt = DateTime.Now
+                };
+
+                history.Add(historyItem);
+                historyService.SaveHistory("history.json", history);
+                Console.WriteLine($"{expression} = {result}");
+                break;
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine($"Error: couldn't compute this expression {ex}"); 
+            }
+        }
+    }
+}
