9cbd3f554396bb6a8db432208e67336cba1754cb
Sunnat <sunnatqayumov92@gmail.com>
2025-05-02 20:34:46 +0500

    program.cs

--- src/Team.Calculator/Program.cs
+++ src/Team.Calculator/Program.cs
@@ -1,1 +1,41 @@
-﻿Console.Write("hello world");
+﻿using Team.Calculator.Services;
+using Team.Calculator.Models;
+
+var historyService = new HistoryService();
+var calculateService = new CalculateService();
+
+Console.WriteLine("Welcome to Calculator!");
+
+List<HistoryItem> history = [];
+
+string filePath = "history.json";
+
+historyService.LoadHistory(filePath, history);
+
+Console.WriteLine("program cs" + history.Count);
+
+while (true)
+{
+    Console.WriteLine("availabe commands: calculate, history, clear, exit, cls");
+
+    var input = Console.ReadLine()!.Trim().ToLower(); 
+
+    if (input == "exit")
+        break;
+
+    else if (input == "cls")
+        Console.Clear();
+
+    else if (input == "history") 
+        historyService.ShowHistory(history);
+
+    else if (input == "clear")
+        historyService.ClearHistory(history);
+    
+    else if (input == "calculate")
+        calculateService.Calculate(history);
+
+    else
+        Console.WriteLine("This command is not available");
+}
+Console.WriteLine("Good bye");
